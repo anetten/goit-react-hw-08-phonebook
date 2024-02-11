1cb087a0cf30035a052fc77f7b264424a8da6f7e
anetten <anet.zvekova@gmail.com>
2024-02-11 14:41:26 +0200

    Update authSlice.operations.js

--- src/redux/auth/authSlice.operations.js
+++ src/redux/auth/authSlice.operations.js
@@ -23,7 +23,7 @@ export const apiRegisterUser = createAsyncThunk(
 
       return data;
     } catch (error) {
-      console.log(error);
+      console.error(error);
       thunkApi.rejectWithValue(error.message);
     }
   }
@@ -38,7 +38,7 @@ export const apiLoginUser = createAsyncThunk(
 
       return data;
     } catch (error) {
-      console.log(error);
+      console.error(error);
       thunkApi.rejectWithValue(error.message);
     }
   }
